29dff48234a5eb6384ebee7f37b00970c04f94e4
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-10 01:55:13 +0800

    Fix swiftlint errors

--- Annotato/Annotato/AppDelegate.swift
+++ Annotato/Annotato/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
