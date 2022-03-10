ffc862c8ac70e09d98c8de5ddae3546eedeccbb9
Darren Heng <darren2pro@gmail.com>
2022-03-10 21:46:45 +0800

    Load pdf view into the main view

--- Annotato/Annotato/ViewController.swift
+++ Annotato/Annotato/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
+    let pdfView = PDFView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(pdfView)
+
+        guard let url = Bundle.main.url(forResource: "Lab01Qns", withExtension: "pdf") else {
+            print("No such pdf")
+            return
+        }
+        guard let document = PDFDocument(url: url) else {
+            print("No such document")
+            return
+        }
+        pdfView.document = document
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pdfView.frame = view.bounds
     }
 
 }
