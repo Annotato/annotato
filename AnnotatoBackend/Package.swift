68028082b9e001622bb868bd953eb205b52deaf8
Hong Yao <ganhongyao@u.nus.edu>
2022-03-12 21:05:18 +0800

    Add Fluent dependency

--- AnnotatoBackend/Package.swift
+++ AnnotatoBackend/Package.swift
@@ -9,14 +9,18 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(name: "AnnotatoSharedLibrary", path: "../AnnotatoSharedLibrary")
+        .package(name: "AnnotatoSharedLibrary", path: "../AnnotatoSharedLibrary"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                "AnnotatoSharedLibrary"
+                "AnnotatoSharedLibrary",
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
