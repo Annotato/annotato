3a60ba59493cad54be2896434c090f54031bfd10
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-14 01:07:54 +0800

    Integrate auth with view

--- Annotato/Annotato/Views/Main/Auth/LogInViewController.swift
+++ Annotato/Annotato/Views/Main/Auth/LogInViewController.swift
@@ -1,30 +1,83 @@
 import UIKit
 
-class LogInViewController: UIViewController, Navigable {
+class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
+    let auth = AnnotatoAuth()
+
+    // Storyboard UI Elements
+    @IBOutlet private var formSegmentedControl: UISegmentedControl!
+    @IBOutlet private var emailTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private var displayNameTextField: UITextField!
+    @IBOutlet private var submitButton: UIButton!
+
+    @IBOutlet private var heightConstraint: NSLayoutConstraint!
+    @IBOutlet private var displayNameContainer: UIView!
+
+    private enum Segment: Int {
+        case signIn
+        case signUp
+    }
+
+    let signInButtonText = "Sign In"
+    let signUpButtonText = "Sign Up"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        initializeSubviews()
+        auth.delegate = self
+        displayNameContainer.isHidden = true
+        heightConstraint.constant = 50
+        passwordTextField.isSecureTextEntry = true
     }
 
-    private func initializeSubviews() {
-        initializeLoginButton()
+    private func updateFormViews() {
+        let segment = Segment(rawValue: formSegmentedControl.selectedSegmentIndex)
+        switch segment {
+        case .signIn:
+            displayNameContainer.isHidden = true
+            heightConstraint.constant = 50
+            submitButton.setTitle(signInButtonText, for: .normal)
+        case .signUp:
+            displayNameContainer.isHidden = false
+            heightConstraint.constant = 30
+            submitButton.setTitle(signUpButtonText, for: .normal)
+        default:
+            fatalError("Invalid Segment")
+        }
     }
 
-    private func initializeLoginButton() {
-        let loginButton = FilledButton(
-            title: "Log In",
-            frame: CGRect(x: .zero, y: .zero, width: 240, height: 40)
-        )
-
-        loginButton.center = view.center
-        loginButton.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
-
-        view.addSubview(loginButton)
+    @IBAction private func onFormActionChanged(_ sender: UISegmentedControl) {
+        updateFormViews()
     }
 
-    @objc
-    private func didTapLoginButton() {
+    @IBAction private func onSubmitButtonTapped(_ sender: UIButton) {
+        let segment = Segment(rawValue: formSegmentedControl.selectedSegmentIndex)
+        switch segment {
+        case .signIn:
+            auth.logIn(email: emailTextField.text ?? "", password: passwordTextField.text ?? "")
+        case .signUp:
+            auth.signUp(email: emailTextField.text ?? "",
+                        password: passwordTextField.text ?? "",
+                        displayName: displayNameTextField.text ?? ""
+            )
+        default:
+            fatalError("Invalid Segment")
+        }
+    }
+
+    func logInDidFail(_ error: Error) {
+        print(error)
+    }
+
+    func signUpDidFail(_ error: Error) {
+        print(error)
+    }
+
+    func logInDidSucceed() {
         goToDocumentList()
+    }
+
+    func signUpDidSucceed() {
+        print("yay")
     }
 }
