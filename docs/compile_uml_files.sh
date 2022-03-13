cd974590457babce27ee02d5ecffbd8700575992
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-14 00:10:16 +0800

    Minor change to script

--- docs/compile_uml_files.sh
+++ docs/compile_uml_files.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+JAR_FILE=plantuml.jar
+
+if [ ! -f "$JAR_FILE" ]; then
+    echo "\n$JAR_FILE does not exist.\n"
+    echo "Download from https://plantuml.com/download"
+    exit
+fi
+
 java -jar plantuml.jar *.puml -o diagrams
 
