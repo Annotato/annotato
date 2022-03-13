a888339cd3246b9421bf74bc45f2d07ead51aa92
Hong Yao <ganhongyao@u.nus.edu>
2022-03-13 17:54:23 +0800

    Add route for testing creation of documents

--- AnnotatoBackend/Sources/App/routes.swift
+++ AnnotatoBackend/Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import AnnotatoSharedLibrary // TODO: Remove after testing
 
 func routes(_ app: Application) throws {
     app.get { _ in
@@ -7,5 +8,15 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { _ -> String in
         "Hello, world!"
+    }
+
+    // TODO: Remove after testing
+    /// For testing only: Creates a document and returns the id of the created document as the response
+    app.get("test_documents") { _ -> String in
+        let testDocument = Document(name: "Test Document", ownerId: UUID(), baseFileUrl: "path/to/document")
+        let testDocumentEntity = DocumentEntity.fromModel(testDocument)
+        testDocumentEntity.save(on: app.db)
+
+        return String(describing: testDocumentEntity.id)
     }
 }
