18fb1271074867b3d30f8ad19fb3a03933a92a87
Hong Yao <ganhongyao@u.nus.edu>
2022-03-13 23:16:05 +0800

    Remove line breaks for consistency

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Document.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Document.swift
@@ -2,11 +2,8 @@ import Foundation
 
 public class Document: Codable {
     public var id: UUID?
-
     public private(set) var name: String
-
     public let ownerId: UUID
-
     public let baseFileUrl: String
 
     public required init(name: String, ownerId: UUID, baseFileUrl: String, id: UUID? = nil) {
