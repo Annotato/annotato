266ee22b3181539d3f8b52a7f1fd387aca8de7f5
Hong Yao <ganhongyao@u.nus.edu>
2022-03-13 23:37:00 +0800

    Remove test route

--- AnnotatoBackend/Sources/App/routes.swift
+++ AnnotatoBackend/Sources/App/routes.swift
@@ -1,5 +1,4 @@
 import Vapor
-import AnnotatoSharedLibrary // TODO: Remove after testing
 
 func routes(_ app: Application) throws {
     app.get { _ in
@@ -8,14 +7,5 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { _ -> String in
         "Hello, world!"
-    }
-
-    // TODO: Remove after testing: Creates a document and returns the id of the created document as the response
-    app.get("test_documents") { _ -> String in
-        let testDocument = Document(name: "Test Document", ownerId: UUID(), baseFileUrl: "path/to/document")
-        let testDocumentEntity = DocumentEntity.fromModel(testDocument)
-        testDocumentEntity.save(on: app.db)
-
-        return String(describing: testDocumentEntity.id)
     }
 }
