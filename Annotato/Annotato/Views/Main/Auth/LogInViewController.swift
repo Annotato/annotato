2613b0a3cfecbc042e8d16891050bc31c2c60741
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-14 16:00:02 +0800

    Complete login view

--- Annotato/Annotato/Views/Main/Auth/LogInViewController.swift
+++ Annotato/Annotato/Views/Main/Auth/LogInViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
-    let auth = AnnotatoAuth()
+class LogInViewController: UIViewController, Navigable {
+    private let auth = AnnotatoAuth()
 
     // Storyboard UI Elements
     @IBOutlet private var formSegmentedControl: UISegmentedControl!
@@ -9,17 +9,16 @@ class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
     @IBOutlet private var passwordTextField: UITextField!
     @IBOutlet private var displayNameTextField: UITextField!
     @IBOutlet private var submitButton: UIButton!
-
     @IBOutlet private var heightConstraint: NSLayoutConstraint!
     @IBOutlet private var displayNameContainer: UIView!
 
     private enum Segment: Int {
-        case signIn
+        case logIn
         case signUp
     }
 
-    let signInButtonText = "Sign In"
-    let signUpButtonText = "Sign Up"
+    private let logInButtonText = "Log In"
+    private let signUpButtonText = "Sign Up"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,35 +26,19 @@ class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
         auth.delegate = self
         displayNameContainer.isHidden = true
         heightConstraint.constant = 50
-        passwordTextField.isSecureTextEntry = true
-    }
-
-    private func updateFormViews() {
-        let segment = Segment(rawValue: formSegmentedControl.selectedSegmentIndex)
-        switch segment {
-        case .signIn:
-            displayNameContainer.isHidden = true
-            heightConstraint.constant = 50
-            submitButton.setTitle(signInButtonText, for: .normal)
-        case .signUp:
-            displayNameContainer.isHidden = false
-            heightConstraint.constant = 30
-            submitButton.setTitle(signUpButtonText, for: .normal)
-        default:
-            fatalError("Invalid Segment")
-        }
-    }
-
-    @IBAction private func onFormActionChanged(_ sender: UISegmentedControl) {
-        updateFormViews()
     }
 
     @IBAction private func onSubmitButtonTapped(_ sender: UIButton) {
         let segment = Segment(rawValue: formSegmentedControl.selectedSegmentIndex)
         switch segment {
-        case .signIn:
+        case .logIn:
             auth.logIn(email: emailTextField.text ?? "", password: passwordTextField.text ?? "")
         case .signUp:
+            guard !(displayNameTextField.text ?? "").isEmpty else {
+                presentErrorAlert(errorMessage: "Display Name cannot be empty.")
+                return
+            }
+
             auth.signUp(email: emailTextField.text ?? "",
                         password: passwordTextField.text ?? "",
                         displayName: displayNameTextField.text ?? ""
@@ -65,12 +48,34 @@ class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
         }
     }
 
+    @IBAction private func onFormActionChanged(_ sender: UISegmentedControl) {
+        updateFormViews()
+    }
+
+    private func updateFormViews() {
+        let segment = Segment(rawValue: formSegmentedControl.selectedSegmentIndex)
+        switch segment {
+        case .logIn:
+            displayNameContainer.isHidden = true
+            heightConstraint.constant = 50
+            submitButton.setTitle(logInButtonText, for: .normal)
+        case .signUp:
+            displayNameContainer.isHidden = false
+            heightConstraint.constant = 30
+            submitButton.setTitle(signUpButtonText, for: .normal)
+        default:
+            fatalError("Invalid Segment")
+        }
+    }
+}
+
+extension LogInViewController: AnnotatoAuthDelegate, AlertPresentable {
     func logInDidFail(_ error: Error) {
-        print(error)
+        presentErrorAlert(errorMessage: error.localizedDescription)
     }
 
     func signUpDidFail(_ error: Error) {
-        print(error)
+        presentErrorAlert(errorMessage: error.localizedDescription)
     }
 
     func logInDidSucceed() {
@@ -78,6 +83,8 @@ class LogInViewController: UIViewController, Navigable, AnnotatoAuthDelegate {
     }
 
     func signUpDidSucceed() {
-        print("yay")
+        presentTimedAlert(title: "Sign Up Successful!", message: "Please log in to use the application.")
+        formSegmentedControl.selectedSegmentIndex = 0
+        updateFormViews()
     }
 }
