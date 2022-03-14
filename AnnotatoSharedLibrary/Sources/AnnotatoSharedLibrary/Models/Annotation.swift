bbe46eecf51b14932075a42880cb650b96f3df05
Darren Heng <darren2pro@gmail.com>
2022-03-14 21:01:33 +0800

    Model for annotation and annotation parts - tentative to changes

---
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Annotation.swift
@@ -0,0 +1,21 @@
+import Foundation
+import CoreGraphics
+
+public class Annotation: Codable {
+    public var id: UUID?
+    public let ownerId: UUID
+    public private(set) var document: Document
+    public var center: CGPoint
+    public var width: Double
+    public var parts: [AnnotationPart]
+
+    public required init(id: UUID? = nil, ownerId: UUID, document: Document,
+                         center: CGPoint, width: Double, parts: [AnnotationPart]) {
+        self.id = id
+        self.ownerId = ownerId
+        self.document = document
+        self.center = center
+        self.width = width
+        self.parts = parts
+    }
+}
---
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/AnnotationPart.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public class AnnotationPart: Codable {
+    public var id: UUID?
+    public var height: Double
+    public var content: String
+    public let annotationType: String //TODO: Change this to enum
+
+    public required init(id: UUID?, height: Double, content: String, annotationType: String) {
+        self.id = id
+        self.height = height
+        self.content = content
+        self.annotationType = annotationType
+    }
+}
