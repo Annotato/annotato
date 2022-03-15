ddf99c7db607b6f71de49c1023427489f9bcd464
Geng Ning <gengninghan1@gmail.com>
2022-03-16 06:17:13 +0800

    Remove more old files

--- Annotato/Annotato/ViewModels/Sample/SampleData.swift
+++ Annotato/Annotato/ViewModels/Sample/SampleData.swift
@@ -49,38 +49,4 @@ class SampleData {
             )
         ]
     }
-
-//    private func exampleAnnotations() -> [DocumentAnnotationViewModel] {
-//        [
-//            DocumentAnnotationViewModel(
-//                center: CGPoint(x: 450.0, y: 150.0),
-//                width: 300.0,
-//                parts: exampleAnnotationParts1()
-//            ),
-//            DocumentAnnotationViewModel(
-//                center: CGPoint(x: 600.0, y: 300.0),
-//                width: 250.0,
-//                parts: exampleAnnotationParts2())
-//        ]
-//    }
-
-//    private func exampleAnnotationParts1() -> [DocumentAnnotationPartViewModel] {
-//        [
-//            DocumentAnnotationTextViewModel(id: UUID(), content: "I am hungry", height: 30.0),
-//            DocumentAnnotationTextViewModel(id: UUID(), content: "ABC\nDEF", height: 60.0)
-//        ]
-//    }
-//
-//    private func exampleAnnotationParts2() -> [DocumentAnnotationPartViewModel] {
-//        [
-//            DocumentAnnotationTextViewModel(
-//                id: UUID(),
-//                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//                height: 44.0),
-//            DocumentAnnotationTextViewModel(
-//                id: UUID(),
-//                content: "Hello\nHello\nHello",
-//                height: 60.0)
-//        ]
-//    }
 }
