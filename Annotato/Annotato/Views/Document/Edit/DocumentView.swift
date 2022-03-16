aecf004fae3c08f2c44eee7b0b3764ca0c18fc39
Darren Heng <darren2pro@gmail.com>
2022-03-17 01:04:09 +0800

    Implement a notification center for onPageChanged

--- Annotato/Annotato/Views/Document/Edit/DocumentView.swift
+++ Annotato/Annotato/Views/Document/Edit/DocumentView.swift
@@ -18,10 +18,62 @@ class DocumentView: UIView {
         self.layer.borderColor = UIColor.black.cgColor
 
         addTapGestureRecognizer()
+        addObservers()
         initializePdfView()
-        initializeAnnotationViews()
+        initializeAnnotationViewsForVisiblePages()
     }
 
+    private func addObservers() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(handlePageChange(notification:)),
+            name: Notification.Name.PDFViewPageChanged, object: nil
+        )
+    }
+
+    @objc
+    private func handlePageChange(notification: Notification) {
+        print("changed pages")
+    }
+
+    private func initializeAnnotationViewsForVisiblePages() {
+        guard let pdfSubView = subviews.first(where: { subview in
+            subview is DocumentPdfView
+        }) else {
+            return
+        }
+        guard let pdfSubView = pdfSubView as? DocumentPdfView else {
+            return
+        }
+        let visiblePages = pdfSubView.visiblePages
+        print(visiblePages)
+        print("-------------------------------------")
+        for annotation in documentViewModel.annotations {
+            let view = DocumentAnnotationView(annotationViewModel: annotation)
+            print(annotation.associatedPage)
+            // MARK: Bug here. Need to find a way to compare PDFPages accurately.
+            // Different instances of the same file will have different memory
+            // locations
+            if visiblePages.contains(annotation.associatedPage) {
+                // Then we should add it to the subview
+                pdfSubView.documentView?.addSubview(view)
+            } else {
+                // we should remove it from the subview if it is there, to avoid
+                // memory leak
+                if subviews.contains(view) {
+                    print("removing a view that is out of the visible pages")
+                    // TODO: Implement removing subviews if they are out of view.
+                    // It needs to be done from the annotation view itself I think
+                    // by calling removeFromSuperView()
+                } else {
+                    print("subviews doesn't contain it, but it is not visible so not added")
+                }
+            }
+        }
+    }
+
+    /*
+     Might not be needed in the future
+     */
     private func initializeAnnotationViews() {
         guard let pdfSubView = subviews.first(where: { subview in
             subview is DocumentPdfView
