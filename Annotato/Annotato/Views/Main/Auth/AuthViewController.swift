6e5e2fd3e0d7f44e3af24de905ecb662688a2ca7
Hong Yao <ganhongyao@u.nus.edu>
2022-03-17 00:51:10 +0800

    Fix lint

--- Annotato/Annotato/Views/Main/Auth/AuthViewController.swift
+++ Annotato/Annotato/Views/Main/Auth/AuthViewController.swift
@@ -58,22 +58,26 @@ class AuthViewController: UIViewController, Navigable {
         let ownerIdOfExistingDocument = "1e959847-f7a5-4a6f-8e33-de7555ea03d2"
 
         Task {
-            let documentsFetched = await DocumentsAPI().getDocuments(userId: UUID(uuidString: ownerIdOfExistingDocument)!)
+            let documentsFetched = await DocumentsAPI()
+                .getDocuments(userId: UUID(uuidString: ownerIdOfExistingDocument)!)
             AnnotatoLogger.info("LIST \(documentsFetched)")
 
-            let documentFetched = await DocumentsAPI().getDocument(documentId: UUID(uuidString: anExistingDocumentId)!)
+            let documentFetched = await DocumentsAPI()
+                .getDocument(documentId: UUID(uuidString: anExistingDocumentId)!)
             AnnotatoLogger.info("GET \(documentFetched)")
 
             let documentToUpdate = Document(name: "Updated name", ownerId: UUID(uuidString: ownerIdOfExistingDocument)!,
-                                       baseFileUrl: "updated/path", id: UUID(uuidString: anExistingDocumentId)!)
+                                            baseFileUrl: "updated/path", id: UUID(uuidString: anExistingDocumentId)!)
             let updatedDocument = await DocumentsAPI().updateDocument(document: documentToUpdate)
             AnnotatoLogger.info("PUT \(updatedDocument)")
 
-            let documentToCreate = Document(name: "Newly created document", ownerId: UUID(), baseFileUrl: "path/to/new/document")
+            let documentToCreate = Document(name: "Newly created document", ownerId: UUID(),
+                                            baseFileUrl: "path/to/new/document")
             let createdDocument = await DocumentsAPI().createDocument(document: documentToCreate)
             AnnotatoLogger.info("POST \(createdDocument)")
 
-            let deletedDocument = await DocumentsAPI().deleteDocument(documentId: UUID(uuidString: "60d8cb78-95c6-4bd7-8392-d64fb0ce3f15")!)
+            let deletedDocument = await DocumentsAPI()
+                .deleteDocument(documentId: UUID(uuidString: "60d8cb78-95c6-4bd7-8392-d64fb0ce3f15")!)
             AnnotatoLogger.info("DELETE \(deletedDocument)")
         }
     }
