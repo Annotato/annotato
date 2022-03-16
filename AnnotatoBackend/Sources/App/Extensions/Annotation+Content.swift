ff0e4c943afbc16dfa8f623dccb3f74fc83c0225
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-16 19:40:31 +0800

    Fix swiftlint errors

--- AnnotatoBackend/Sources/App/Extensions/Annotation+Content.swift
+++ AnnotatoBackend/Sources/App/Extensions/Annotation+Content.swift
@@ -3,5 +3,5 @@ import AnnotatoSharedLibrary
 
 /// Allows Annotation to be encoded as a response.
 extension Annotation: Content {
-    
+
 }
--- AnnotatoBackend/Sources/App/Extensions/AnnotationText+Content.swift
+++ AnnotatoBackend/Sources/App/Extensions/AnnotationText+Content.swift
@@ -3,5 +3,5 @@ import AnnotatoSharedLibrary
 
 /// Allows AnnotationText to be encoded as a response.
 extension AnnotationText: Content {
-    
+
 }
