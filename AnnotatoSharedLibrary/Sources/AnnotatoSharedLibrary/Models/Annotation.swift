57761e0641eda255e4f8c6fa14cda3a546dbb5a4
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-17 17:49:07 +0800

    Add documentId to annotation

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Annotation.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Annotation.swift
@@ -5,12 +5,14 @@ public final class Annotation: Codable {
     public let id: UUID?
     public private(set) var origin: CGPoint
     public private(set) var width: Double
-    public let ownerId: UUID
+    public let ownerId: String
+    public let documentId: UUID
 
-    public required init(origin: CGPoint, width: Double, ownerId: UUID, id: UUID? = nil) {
+    public required init(origin: CGPoint, width: Double, ownerId: String, documentId: UUID, id: UUID? = nil) {
         self.id = id
         self.origin = origin
         self.width = width
         self.ownerId = ownerId
+        self.documentId = documentId
     }
 }
