f025e8814780a38754f0148441b1093a084745c6
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-18 14:38:55 +0800

    Add annotations to document model

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Document.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Document.swift
@@ -5,17 +5,19 @@ public final class Document: Codable {
     public private(set) var name: String
     public let ownerId: String
     public let baseFileUrl: String
+    public private(set) var annotations: [Annotation]
 
-    public required init(id: UUID? = nil, name: String, ownerId: String, baseFileUrl: String) {
+    public required init(id: UUID? = nil, name: String, ownerId: String, baseFileUrl: String, annotations: [Annotation] = []) {
         self.id = id ?? UUID()
         self.name = name
         self.ownerId = ownerId
         self.baseFileUrl = baseFileUrl
+        self.annotations = annotations
     }
 }
 
 extension Document: CustomStringConvertible {
     public var description: String {
-        "Document(id: \(id), name: \(name), ownerId: \(ownerId), baseFileUrl: \(baseFileUrl))"
+        "Document(id: \(id), name: \(name), ownerId: \(ownerId), baseFileUrl: \(baseFileUrl), annotations: \(annotations)"
     }
 }
