921712c60935d10ba583b92c160cfc09617275ad
Hong Yao <ganhongyao@u.nus.edu>
2022-03-18 12:27:24 +0800

    Remove test code

--- Annotato/Annotato/Views/Main/Auth/AuthViewController.swift
+++ Annotato/Annotato/Views/Main/Auth/AuthViewController.swift
@@ -1,5 +1,4 @@
 import UIKit
-import AnnotatoSharedLibrary // TODO: TO REMOVE AFTER TEST
 
 class AuthViewController: UIViewController, Navigable {
     private let auth = AnnotatoAuth()
@@ -82,16 +81,6 @@ extension AuthViewController: AnnotatoAuthDelegate, AlertPresentable {
 
     func logInDidSucceed() {
         goToDocumentList()
-
-        // TODO: REMOVE THESE TEST LINES
-        let storage = AnnotatoPdfStorageManager()
-        let fileUrl = Bundle.main.url(forResource: "random", withExtension: ".pdf")!
-        storage.uploadPdf(fileSystemUrl: fileUrl, withName: "random.pdf") { document in
-            print("Uploaded document", document)
-            storage.deletePdf(document: document, completion: { document in
-                print("Deleted document", document)
-            })
-        }
     }
 
     func signUpDidSucceed() {
