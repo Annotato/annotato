85d7ec2b6ab2cf32febb6ade3c9eeb73a6312ec9
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-20 14:43:05 +0800

    Update documentation

--- Annotato/Annotato/Views/Components/Extensions/WKWebView.swift
+++ Annotato/Annotato/Views/Components/Extensions/WKWebView.swift
@@ -2,9 +2,9 @@ import WebKit
 
 // References: https://stackoverflow.com/questions/45998220/the-font-looks-like-smaller-in-wkwebview-than-in-uiwebview
 extension WKWebView {
-    /// load HTML String same font like the UIWebview
+    /// Load HTML String same font like the UIWebview
     ///
-    //// - Parameters:
+    /// - Parameters:
     ///   - content: HTML content which we need to load in the webview.
     ///   - baseURL: Content base url. It is optional.
     func loadHTMLStringWithCorrectScale(content: String, baseURL: URL?) {
