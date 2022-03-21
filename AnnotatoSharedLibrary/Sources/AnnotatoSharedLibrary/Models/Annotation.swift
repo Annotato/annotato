1be5e651277efe4beee56f82fdc74c5659d097bf
Geng Ning <gengninghan1@gmail.com>
2022-03-21 20:21:26 +0800

    Fix swiftlint

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Annotation.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/Annotation.swift
@@ -24,7 +24,7 @@ public final class Annotation: Codable {
         self.ownerId = ownerId
         self.documentId = documentId
     }
-    
+
     private enum CodingKeys: String, CodingKey {
         case id
         case origin
@@ -33,7 +33,7 @@ public final class Annotation: Codable {
         case ownerId
         case documentId
     }
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(UUID.self, forKey: .id)
@@ -41,13 +41,13 @@ public final class Annotation: Codable {
         width = try container.decode(Double.self, forKey: .width)
         ownerId = try container.decode(String.self, forKey: .ownerId)
         documentId = try container.decode(UUID.self, forKey: .documentId)
-        
+
         // Note: AnnotationPart protocol has to be split into concrete types to decode
         parts = []
         let texts = try container.decode([AnnotationText].self, forKey: .texts)
         parts.append(contentsOf: texts)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
@@ -55,7 +55,7 @@ public final class Annotation: Codable {
         try container.encode(width, forKey: .width)
         try container.encode(ownerId, forKey: .ownerId)
         try container.encode(documentId, forKey: .documentId)
-        
+
         // Note: AnnotationPart protocol has to be split into concrete types to encode
         let texts = parts.compactMap({ $0 as? AnnotationText })
         try container.encode(texts, forKey: .texts)
@@ -64,6 +64,7 @@ public final class Annotation: Codable {
 
 extension Annotation: CustomDebugStringConvertible {
     public var debugDescription: String {
-        "Annotation(id: \(id), origin: \(origin), width: \(width), parts: \(parts), ownerId: \(ownerId), documentId: \(documentId))"
+        "Annotation(id: \(id), origin: \(origin), width: \(width), " +
+        "parts: \(parts), ownerId: \(ownerId), documentId: \(documentId))"
     }
 }
