a403264e6a15f29acfbe43506c9c664577505e16
Hong Yao <ganhongyao@u.nus.edu>
2022-03-21 19:41:07 +0800

    Add order property to annotation text

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/AnnotationText.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/AnnotationText.swift
@@ -5,12 +5,14 @@ public final class AnnotationText: Codable {
     public let type: AnnotationType
     public private(set) var content: String
     public private(set) var height: Double
+    public private(set) var order: Int
     public let annotationId: UUID
 
     public required init(
         type: AnnotationType,
         content: String,
         height: Double,
+        order: Int,
         annotationId: UUID,
         id: UUID? = nil
     ) {
@@ -18,6 +20,7 @@ public final class AnnotationText: Codable {
         self.type = type
         self.content = content
         self.height = height
+        self.order = order
         self.annotationId = annotationId
     }
 }
@@ -25,6 +28,6 @@ public final class AnnotationText: Codable {
 extension AnnotationText: CustomDebugStringConvertible {
     public var debugDescription: String {
         "AnnotationText(id: \(id), type: \(type), content: \(content), " +
-        "height: \(height), annotationId: \(annotationId))"
+        "height: \(height), order: \(order), annotationId: \(annotationId))"
     }
 }
