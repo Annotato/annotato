4e1ee2e6475d53f6169dd8cc5152af08392bf8c5
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-27 18:31:15 +0800

    Add WebSocketManager

--- Annotato/Annotato/WebSocket/WebSocketManager.swift
+++ Annotato/Annotato/WebSocket/WebSocketManager.swift
@@ -1,8 +1,47 @@
-//
-//  WebSocketManager.swift
-//  Annotato
-//
-//  Created by Sivayogasubramanian on 27/3/22.
-//
-
 import Foundation
+
+class WebSocketManager {
+    var socket: URLSessionWebSocketTask?
+
+    func setSocket(to session: URLSessionWebSocketTask) {
+        socket = session
+        listen()
+        socket?.resume()
+
+        print("Set socket")
+    }
+
+    func resetSocket() {
+        socket = nil
+    }
+
+    func listen() {
+        guard let socket = socket else {
+            return
+        }
+
+        socket.receive { [weak self] result in
+            guard let self = self else {
+                return
+            }
+
+            print(result)
+
+            switch result {
+            case .failure(let error):
+                AnnotatoLogger.error(error.localizedDescription)
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    print(data)
+                case .string(let str):
+                    print(str)
+                @unknown default:
+                    break
+                }
+            }
+
+            self.listen()
+        }
+    }
+}
