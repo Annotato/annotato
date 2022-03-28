0958d1e8e91061008efbafd52a79cd53213aca10
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-28 21:43:22 +0800

    Use local http endpoint

--- Annotato/Annotato/API/BaseAPI.swift
+++ Annotato/Annotato/API/BaseAPI.swift
@@ -3,7 +3,8 @@ struct BaseAPI {
     private static let localApiEndpoint = "http://127.0.0.1:8080"
     private static let remoteApiEndpoint = "http://178.128.111.22:80"
 
-    static let baseAPIUrl = remoteApiEndpoint
+    // TODO: CHANGE TO REMOTE ENDPOINT BEFORE MERGE
+    static let baseAPIUrl = localApiEndpoint
 
     // WebSocket
     private static let localApiWsEnpoint = "ws://127.0.0.1:8080"
