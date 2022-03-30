c27e2040013666d8eb7a9418fd194b219ef0b82a
Geng Ning <gengninghan1@gmail.com>
2022-03-30 14:04:33 +0800

    Use RichTextView

--- Annotato/Annotato/Views/Adapters/RichTextView.swift
+++ Annotato/Annotato/Views/Adapters/RichTextView.swift
@@ -3,7 +3,18 @@ import UIKit
 
 extension RichTextView: AnnotatoMarkdownService {
     func renderMarkdown(from input: String, frame: CGRect) -> UIView {
-        RichTextView(
+        let containerView = UIView(frame: frame)
+
+        let leftPadding = 5.0
+        let topPadding = 5.0
+        let richTextFrame = CGRect(
+            x: leftPadding,
+            y: topPadding,
+            width: frame.width - leftPadding * 2,
+            height: frame.height - topPadding * 2
+        )
+
+        let richTextView = RichTextView(
             input: input,
             latexParser: LatexParser(),
             font: UIFont.systemFont(ofSize: UIFont.systemFontSize),
@@ -13,8 +24,11 @@ extension RichTextView: AnnotatoMarkdownService {
             latexTextBaselineOffset: 0,
             interactiveTextColor: UIColor.blue,
             textViewDelegate: nil,
-            frame: frame,
+            frame: richTextFrame,
             completion: nil
         )
+
+        containerView.addSubview(richTextView)
+        return containerView
     }
 }
