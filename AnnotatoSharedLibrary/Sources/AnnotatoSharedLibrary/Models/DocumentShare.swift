be192efc3e5e4fdfa68d5c218cbffffbb6393361
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-03-31 07:20:01 +0800

    Make DocumentShare id non-optional

--- AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/DocumentShare.swift
+++ AnnotatoSharedLibrary/Sources/AnnotatoSharedLibrary/Models/DocumentShare.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public final class DocumentShare: Codable {
-    public let id: UUID?
+    public let id: UUID
     public private(set) var documentId: UUID
     public private(set) var recipientId: String
 
@@ -10,7 +10,7 @@ public final class DocumentShare: Codable {
         recipientId: String,
         id: UUID? = nil
     ) {
-        self.id = id
+        self.id = id ?? UUID()
         self.documentId = documentId
         self.recipientId = recipientId
     }
