23b53f3287bcf703c2c467f5b938b79a886fbc23
Geng Ning <gengninghan1@gmail.com>
2022-03-31 12:37:21 +0800

    Add offline to online controller

---
+++ AnnotatoBackend/Sources/App/WebSocketControllers/OfflineToOnlineWebSocketController.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Vapor
+import AnnotatoSharedLibrary
+import FluentKit
+
+class OfflineToOnlineWebSocketController {
+    private static var logger = Logger(label: "OfflineToOnlineWebSocketController")
+
+    static func handleOfflineToOnlineResolution(userId: String, data: Data, db: Database) async {
+        do {
+            Self.logger.info("Processing offline to online data...")
+
+            let message = try JSONDecoder().decode(AnnotatoOfflineToOnlineMessage.self, from: data)
+
+            switch message.mergeStrategy {
+            case .duplicateConflicts:
+                print("duplicate conflicts")
+            case .keepServerVersion:
+                print("keep server version")
+            case .overrideServerVersion:
+                print("override server version")
+            }
+        } catch {
+            Self.logger.error("Error when handling incoming offline to online data. \(error.localizedDescription)")
+        }
+    }
+}
