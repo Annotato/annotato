cd6fc0b643570f86eca5286815d8a16fd5627720
Geng Ning <gengninghan1@gmail.com>
2022-04-01 01:13:42 +0800

    Change back to remote endpoint

--- Annotato/Annotato/Persistence/RemotePersistenceManager/RemotePersistenceManager.swift
+++ Annotato/Annotato/Persistence/RemotePersistenceManager/RemotePersistenceManager.swift
@@ -3,13 +3,13 @@ struct RemotePersistenceManager {
     private static let localApiEndpoint = "http://127.0.0.1:8080"
     private static let remoteApiEndpoint = "http://178.128.111.22:80"
 
-    static let baseAPIUrl = localApiEndpoint
+    static let baseAPIUrl = remoteApiEndpoint
 
     // WebSocket
     private static let localApiWsEndpoint = "ws://127.0.0.1:8080"
     private static let remoteApiWsEndpoint = "ws://178.128.111.22:80"
 
-    static let baseWsAPIUrl = localApiWsEndpoint
+    static let baseWsAPIUrl = remoteApiWsEndpoint
 }
 
 extension RemotePersistenceManager: PersistenceManager {
