5203b43a5f4b5fe32905c228d59c80f072f3af97
Geng Ning <gengninghan1@gmail.com>
2022-03-31 20:54:33 +0800

    Add timestamps to SelectionBox

--- AnnotatoBackend/Sources/App/Persistence/SharedLibraryModels+Persistable/SelectionBox+Persistable.swift
+++ AnnotatoBackend/Sources/App/Persistence/SharedLibraryModels+Persistable/SelectionBox+Persistable.swift
@@ -8,7 +8,10 @@ extension SelectionBox: Persistable {
             startPoint: CGPoint(x: managedEntity.startPointX, y: managedEntity.startPointY),
             endPoint: CGPoint(x: managedEntity.endPointX, y: managedEntity.endPointY),
             annotationId: managedEntity.$annotationEntity.id,
-            id: managedEntity.id
+            id: managedEntity.id,
+            createdAt: managedEntity.createdAt,
+            updatedAt: managedEntity.updatedAt,
+            deletedAt: managedEntity.deletedAt
         )
     }
 }
