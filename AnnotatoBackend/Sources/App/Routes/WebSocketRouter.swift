639f36a0c21488681d70872458051f258d2a897d
Siva <sivayogasubramanian@gmail.com>
2022-04-04 02:47:09 +0800

    Update max frame size

--- AnnotatoBackend/Sources/App/Routes/WebSocketRouter.swift
+++ AnnotatoBackend/Sources/App/Routes/WebSocketRouter.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func webSocketRouter(ws: RoutesBuilder) {
     ws.group(":userId") { user in
-        user.webSocket(maxFrameSize: WebSocketMaxFrameSize(integerLiteral: 1 << 32),
+        user.webSocket(maxFrameSize: WebSocketMaxFrameSize(integerLiteral: 1 << 31),
                        onUpgrade: WebSocketController.handleIncomingConnection)
     }
 }
