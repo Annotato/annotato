ba03649392697f784f05ee17b28d7be2f23954e8
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-04-05 17:53:54 +0800

    Fix connectivity

--- Annotato/Annotato/Utils/NetworkMonitor.swift
+++ Annotato/Annotato/Utils/NetworkMonitor.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Network
 
 class NetworkMonitor {
@@ -9,11 +10,50 @@ class NetworkMonitor {
     func start() {
         monitor.start(queue: DispatchQueue.global())
         monitor.pathUpdateHandler = { [weak self] path in
-            self?.isConnected = path.status == .satisfied
+            AnnotatoLogger.info("Detected a change in internet connection status...")
+
+            guard let self = self else {
+                return
+            }
+
+            self.isConnected = path.status == .satisfied
+            AnnotatoLogger.info("Setting isConnected to \(self.isConnected)")
+
+            if !self.isConnected {
+                let date = Date()
+                AnnotatoLogger.info("Setting last online date to \(date)")
+                self.setLastOnlineDatetime(to: date)
+            }
         }
     }
 
     func stop() {
         monitor.cancel()
+    }
+}
+
+// MARK: Storing last online time in UserDefaults
+extension NetworkMonitor {
+    private static let lastOnlineDatetimeKey = "lastOnlineDatetime"
+
+    private func setLastOnlineDatetime(to lastOnlineDatetime: Date) {
+        UserDefaults.standard.set(lastOnlineDatetime, forKey: Self.lastOnlineDatetimeKey)
+    }
+
+    func getLastOnlineDatetime() -> Date? {
+        if isConnected {
+            return Date()
+        }
+
+        guard let lastOnlineDatetime = UserDefaults.standard.object(
+            forKey: Self.lastOnlineDatetimeKey
+        ) as? Date else {
+            let value = UserDefaults.standard.object(forKey: Self.lastOnlineDatetimeKey)
+            AnnotatoLogger.error("Failed to read last online from user defaults. We get: \(String(describing: value))",
+                                 context: "NetworkMonitor::getLastOnline")
+            return nil
+        }
+
+        return lastOnlineDatetime
     }
 }
--- Annotato/Annotato/WebSocket/WebSocketManager.swift
+++ Annotato/Annotato/WebSocket/WebSocketManager.swift
@@ -3,14 +3,10 @@ import AnnotatoSharedLibrary
 
 class WebSocketManager {
     static let shared = WebSocketManager()
-    private static let unavailableDestinationHostErrorCode = 54
-    private static let unconnectedSocketErrorCode = 57
-    private static let connectionTimeOutErrorCode = 60
 
     private(set) var socket: URLSessionWebSocketTask?
     let documentManager = DocumentWebSocketManager()
     let annotationManager = AnnotationWebSocketManager()
-    private(set) var isConnected = false
 
     private init() { }
 
@@ -41,38 +37,26 @@ class WebSocketManager {
     func listen() {
         AnnotatoLogger.info("Websocket connection listening for events...")
 
-        // swiftlint:disable closure_body_length
         socket?.receive { [weak self] result in
-            guard let self = self else {
-                return
-            }
             switch result {
             case .failure(let error):
                 AnnotatoLogger.error(error.localizedDescription)
-                let errorCode = (error as NSError).code
-                if errorCode == WebSocketManager.unavailableDestinationHostErrorCode ||
-                    errorCode == WebSocketManager.unconnectedSocketErrorCode ||
-                    errorCode == WebSocketManager.connectionTimeOutErrorCode {
-                    self.storeLastOnlineLocally(to: Date())
-                    self.isConnected = false
-                }
             case .success(let message):
-                self.isConnected = true
                 switch message {
                 case .data(let data):
-                    self.handleResponseData(data: data)
+                    self?.handleResponseData(data: data)
                 case .string(let str):
                     guard let data = str.data(using: .utf8) else {
                         return
                     }
-                    self.handleResponseData(data: data)
+                    self?.handleResponseData(data: data)
                 @unknown default:
                     break
                 }
             }
 
             // We need to re-register the callback closure after a message is received
-            self.listen()
+            self?.listen()
         }
     }
 
@@ -119,28 +103,5 @@ class WebSocketManager {
                 context: "WebSocketManager:handleResponseData:"
             )
         }
-    }
-}
-
-// MARK: Storing last online time in UserDefaults
-extension WebSocketManager {
-    private static let lastOnlineKey = "lastOnline"
-
-    private func storeLastOnlineLocally(to lastOnline: Date) {
-        UserDefaults.standard.set(lastOnline, forKey: WebSocketManager.lastOnlineKey)
-    }
-
-    func getLastOnline() -> Date? {
-        if isConnected {
-            return Date()
-        }
-        guard let lastOnline = UserDefaults.standard.object(
-            forKey: WebSocketManager.lastOnlineKey) as? Date else {
-            let value = UserDefaults.standard.object(forKey: WebSocketManager.lastOnlineKey)
-            AnnotatoLogger.error("Failed to read last online from user defaults. We get: \(String(describing: value))",
-                                 context: "WebSocketManager::getLastOnline")
-            return nil
-        }
-        return lastOnline
     }
 }
